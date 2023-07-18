aa700e18241703c3c33d9d6d4619808b2f890aa0
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2023-07-18 05:04:37 +0000

    initial commit

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,5 @@
 ##############################################################################
-# Resource group
+# Resource Group
 ##############################################################################
 
 module "resource_group" {
@@ -11,14 +11,13 @@ module "resource_group" {
 }
 
 ##############################################################################
-# COS instance
+# RabbitMQ
 ##############################################################################
 
-resource "ibm_resource_instance" "cos_instance" {
-  name              = "${var.prefix}-cos"
+module "icd_rabbitmq" {
+  source            = "../.."
   resource_group_id = module.resource_group.resource_group_id
-  service           = "cloud-object-storage"
-  plan              = "standard"
-  location          = "global"
+  instance_name     = "${var.prefix}-rabbitmq"
+  region            = var.region
   tags              = var.resource_tags
 }
--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,49 @@
 # Outputs
 ##############################################################################
 
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#}
+output "id" {
+  description = "RabbitMQ instance id"
+  value       = ibm_database.rabbitmq_database.id
+}
+
+output "guid" {
+  description = "RabbitMQ instance guid"
+  value       = ibm_database.rabbitmq_database.guid
+}
+
+output "version" {
+  description = "RabbitMQ instance version"
+  value       = ibm_database.rabbitmq_database.version
+}
+
+output "crn" {
+  description = "RabbitMQ instance crn"
+  value       = ibm_database.rabbitmq_database.resource_crn
+}
+
+output "cbr_rule_ids" {
+  description = "CBR rule ids created to restrict RabbitMQ"
+  value       = module.cbr_rule[*].rule_id
+}
+
+output "service_credentials_json" {
+  description = "Service credentials json map"
+  value       = local.service_credentials_json
+  sensitive   = true
+}
+
+output "service_credentials_object" {
+  description = "Service credentials object"
+  value       = local.service_credentials_object
+  sensitive   = true
+}
+
+output "hostname" {
+  description = "Database hostname. Only contains value when var.service_credential_names or var.users are set."
+  value       = length(var.service_credential_names) > 0 ? nonsensitive(ibm_resource_key.service_credentials[keys(var.service_credential_names)[0]].credentials["connection.https.hosts.0.hostname"]) : length(var.users) > 0 ? nonsensitive(flatten(data.ibm_database_connection.database_connection[0].https[0].hosts[0].hostname)) : null
+}
+
+output "port" {
+  description = "Database port. Only contains value when var.service_credential_names or var.users are set."
+  value       = length(var.service_credential_names) > 0 ? nonsensitive(ibm_resource_key.service_credentials[keys(var.service_credential_names)[0]].credentials["connection.https.hosts.0.port"]) : length(var.users) > 0 ? nonsensitive(flatten(data.ibm_database_connection.database_connection[0].https[0].hosts[0].port)) : null
+}
