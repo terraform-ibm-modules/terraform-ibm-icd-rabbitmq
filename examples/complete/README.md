46baee5dbf2a705759b95ddd846a21077228b1fb
Aayush-Abhyarthi <122350533+Aayush-Abhyarthi@users.noreply.github.com>
2023-07-27 14:02:58 +0530

    feat: initial release (#2)

--- examples/complete/README.md
+++ examples/complete/README.md
@@ -1,4 +1,10 @@
-# Complete example
+# Complete example with BYOK encryption, CBR rules, autoscaling, and service credentials creation
 
-<!-- There is a pre-commit hook that will take the title of each example add include it in the repos main README.md  -->
-<!-- Add text below should describe exactly what resources are provisioned / configured by the example  -->
+An end-to-end example that does the following:
+
+- Create a new resource group if one is not passed in.
+- Create Key Protect instance with root key.
+- Create a new Messages for RabbitMQ instance with BYOK encryption and autoscaling enabled.
+- Create service credentials for the instance.
+- Create a Virtual Private Cloud (VPC).
+- Create Context Based Restriction (CBR) to only allow RabbitMQ to be accessible from the VPC.
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -10,10 +10,46 @@ variable "region" {
   default     = "us-south"
 }
 
+variable "admin_pass" {
+  type        = string
+  default     = null
+  sensitive   = true
+  description = "The password for the database administrator. If the admin password is null then the admin user ID cannot be accessed. More users can be specified in a user block. The admin password must be in the range of 10-32 characters."
+}
+
+variable "users" {
+  type = list(object({
+    name     = string
+    password = string
+    type     = string
+    role     = optional(string)
+  }))
+  default     = []
+  sensitive   = true
+  description = "A list of users that you want to create on the database. Multiple blocks are allowed. The user password must be in the range of 10-32 characters."
+}
+
+variable "service_credential_names" {
+  description = "Map of name, role for service credentials that you want to create for the database"
+  type        = map(string)
+  default = {
+    "rabbit_admin" : "Administrator",
+    "rabbit_operator" : "Operator",
+    "rabbit_viewer" : "Viewer",
+    "rabbit_editor" : "Editor",
+  }
+}
+
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example"
-  default     = "complete"
+  default     = "complete-rabbitmq"
+}
+
+variable "rabbitmq_version" {
+  type        = string
+  description = "Version of rabbitmq to deploy"
+  default     = null
 }
 
 variable "resource_group" {
@@ -26,4 +62,39 @@ variable "resource_tags" {
   type        = list(string)
   description = "Optional list of tags to be added to created resources"
   default     = []
+}
+
+variable "auto_scaling" {
+  type = object({
+    disk = object({
+      capacity_enabled             = optional(bool)
+      free_space_less_than_percent = optional(number)
+      io_above_percent             = optional(number)
+      io_enabled                   = optional(bool)
+      io_over_period               = optional(string)
+      rate_increase_percent        = optional(number)
+      rate_limit_mb_per_member     = optional(number)
+      rate_period_seconds          = optional(number)
+      rate_units                   = optional(string)
+    })
+    memory = object({
+      io_above_percent         = optional(number)
+      io_enabled               = optional(bool)
+      io_over_period           = optional(string)
+      rate_increase_percent    = optional(number)
+      rate_limit_mb_per_member = optional(number)
+      rate_period_seconds      = optional(number)
+      rate_units               = optional(string)
+    })
+  })
+  description = "Configure rules to allow your database to automatically increase its resources. Single block of autoscaling is allowed at once."
+  default = {
+    disk = {
+      capacity_enabled : true,
+      io_enabled : true
+    }
+    memory = {
+      io_enabled : true,
+    }
+  }
 }
