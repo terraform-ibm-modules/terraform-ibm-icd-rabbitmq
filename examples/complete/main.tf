8c5fdd5402e6f0d05aa12cee16139892272dde6b
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-02-09 20:09:18 +0000

    chore(deps): update terraform terraform-ibm-modules/key-protect-all-inclusive/ibm to v4.6.0 (#118)
    * chore(deps): update terraform terraform-ibm-modules/key-protect-all-inclusive/ibm to v4.6.0
    
    * fix: inputs and outputs on kms
    
    * SKIP UPGRADE TEST
    
    ---------
    
    Co-authored-by: Steve Peggs <peggs@uk.ibm.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,14 +16,23 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
-  version           = "4.4.2"
+  version           = "4.6.0"
   resource_group_id = module.resource_group.resource_group_id
   # Note: Database instance and Key Protect must be created in the same region when using BYOK
   # See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
   resource_tags             = var.resource_tags
-  key_map                   = { "icd" = ["${var.prefix}-rabbitmq"] }
+  keys = [
+    {
+      key_ring_name = "icd"
+      keys = [
+        {
+          key_name = "${var.prefix}-rabbitmq"
+        }
+      ]
+    }
+  ]
 }
 
 ##############################################################################
@@ -76,7 +85,7 @@ module "icd_rabbitmq" {
   instance_name              = "${var.prefix}-rabbitmq"
   region                     = var.region
   kms_encryption_enabled     = true
-  existing_kms_instance_guid = module.key_protect_all_inclusive.key_protect_guid
+  existing_kms_instance_guid = module.key_protect_all_inclusive.kms_guid
   service_credential_names   = var.service_credential_names
   admin_pass                 = var.admin_pass
   users                      = var.users
