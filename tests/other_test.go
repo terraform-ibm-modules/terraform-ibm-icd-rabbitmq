78a1898e7846b9a552d76f8ed67adc8b8d3ffe8c
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-02-27 12:27:11 +0530

    test: add new test to check for stable version support

--- tests/other_test.go
+++ tests/other_test.go
@@ -17,9 +17,36 @@ func TestRunBasicExample(t *testing.T) {
 		Prefix:             "rabbitmq-basic",
 		ResourceGroup:      resourceGroup,
 		BestRegionYAMLPath: regionSelectionPath,
+		CloudInfoService:   sharedInfoSvc,
 	})
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
+}
+
+func testPlanICDVersions(t *testing.T, version string) {
+	t.Parallel()
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: "examples/basic",
+		TerraformVars: map[string]interface{}{
+			"rabbitmq_version": version,
+		},
+		CloudInfoService: sharedInfoSvc,
+	})
+	output, err := options.RunTestPlan()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
+
+func TestPlanICDVersions(t *testing.T) {
+	t.Parallel()
+
+	// This test will run a terraform plan on available stable versions of rabbitmq
+	versions, _ := sharedInfoSvc.GetAvailableIcdVersions("rabbitmq")
+	for _, version := range versions {
+		t.Run(version, func(t *testing.T) { testPlanICDVersions(t, version) })
+	}
 }
