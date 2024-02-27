1dcaa910291300e620565c2ea263dfc3b5226b35
Aashiq-J <122446118+Aashiq-J@users.noreply.github.com>
2024-02-27 16:58:59 +0530

    feat: add support for rabitmq version 3.12 (#128)

--- variables.tf
+++ variables.tf
@@ -54,9 +54,10 @@ variable "rabbitmq_version" {
   validation {
     condition = anytrue([
       var.rabbitmq_version == null,
-      var.rabbitmq_version == "3.11"
+      var.rabbitmq_version == "3.11",
+      var.rabbitmq_version == "3.12"
     ])
-    error_message = "Version must be 3.11. If no value passed, the current ICD preferred version is used."
+    error_message = "Version must be 3.11 or 3.12. If no value passed, the current ICD preferred version is used."
   }
 }
 
