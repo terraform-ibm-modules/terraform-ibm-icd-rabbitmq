aabe53853f7fe97ee4e51b47dc623a2e358218d2
Akash Kumar <akash.kumar@ibm.com>
2024-03-10 15:02:52 +0530

    small fixes

--- examples/backup/main.tf
+++ examples/backup/main.tf
@@ -34,7 +34,7 @@ data "ibm_database_backups" "backup_database" {
 module "restored_rmq_db" {
   source            = "../.."
   resource_group_id = module.resource_group.resource_group_id
-  instance_name     = "${var.prefix}-rabbitmq"
+  instance_name     = "${var.prefix}-rabbitmq-restored"
   region            = var.region
   rabbitmq_version  = var.rabbitmq_version
   access_tags       = var.access_tags
