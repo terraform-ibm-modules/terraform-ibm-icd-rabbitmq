f32e5b943fcc2724f4f1e7eaa5134a6739d5bbf5
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-05-12 15:45:26 +0530

    add data block

--- main.tf
+++ main.tf
@@ -305,8 +305,14 @@ resource "ibm_database" "rabbitmq_database" {
   }
 }
 
+# Check whether access tags are valid and exist in the account
+data "ibm_iam_access_tag" "access_tags" {
+  for_each = length(var.access_tags) != 0 ? toset(var.access_tags) : []
+  name     = each.value
+}
+
 resource "ibm_resource_tag" "rabbitmq_tag" {
-  depends_on  = [data.ibm_iam_access_tag.access_tag] # Force dependency on data source validation to ensure access_tags exist and are valid before use.
+  depends_on  = [data.ibm_iam_access_tag.access_tags] # Force dependency on data source validation to ensure access_tags exist and are valid before use.
   count       = length(var.access_tags) == 0 ? 0 : 1
   resource_id = ibm_database.rabbitmq_database.resource_crn
   tags        = var.access_tags
