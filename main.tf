62474e89e4907c14f954df6d00bc77b137c237fd
zikra-iqbal <zikra.iqbalmnnit@gmail.com>
2026-07-17 18:38:06 +0530

    feat: gen2 ICD support

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ locals {
 
   # Determine if host_flavor is used
   host_flavor_set = var.member_host_flavor != null ? true : false
+
+  # Determine if gen2 plan is being used
+  is_gen2    = can(regex("-gen2$", var.plan))
+  is_classic = !local.is_gen2 # For code readability and maintenance
 }
 
 ########################################################################################################################
@@ -155,12 +159,19 @@ resource "time_sleep" "wait_for_backup_kms_authorization_policy" {
 # RabbitMQ instance
 ########################################################################################################################
 
+# Workaround:
+# Montreal does not have ICD classic endpoint, so default to Toronto. This stops the module erroring,
+# but it gets the classic versions and not the gen2 versions.
+# This MAY be wrong and result in an apply time failure IF the user specifies gen2 and an unsupported version.
+# ref: https://github.com/terraform-ibm-modules/terraform-ibm-common-utilities/issues/157
 module "available_versions" {
 
   source   = "terraform-ibm-modules/common-utilities/ibm//modules/icd-versions"
   version  = "1.9.0"
-  region   = var.region
+  region   = var.region == "ca-mon" ? "ca-tor" : var.region
   icd_type = "rabbitmq"
+  plan     = var.plan
+  service  = "messages-for-rabbitmq"
 }
 
 
@@ -366,10 +377,11 @@ module "cbr_rule" {
 resource "ibm_resource_key" "service_credentials" {
   for_each             = { for key in var.service_credential_names : key.name => key }
   name                 = each.key
-  role                 = each.value.role
+  role                 = local.is_classic ? each.value.role : null
   resource_instance_id = ibm_database.rabbitmq_database.id
   parameters = {
     service-endpoints = each.value.endpoint
+    role_crn          = local.is_gen2 ? "crn:v1:bluemix:public:iam::::role:${each.value.role}" : null
   }
 }
 
@@ -381,20 +393,21 @@ locals {
   } : null
 
   service_credentials_object = length(var.service_credential_names) > 0 ? {
-    hostname    = ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.https.hosts.0.hostname"]
-    certificate = ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.https.certificate.certificate_base64"]
-    port        = ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.https.hosts.0.port"]
+    hostname    = can(ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.mqtts.hosts.0.hostname"]) ? ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.mqtts.hosts.0.hostname"] : null
+    certificate = can(ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.mqtts.certificate.certificate_base64"]) ? ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.mqtts.certificate.certificate_base64"] : null
+    port        = can(ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.mqtts.hosts.0.port"]) ? ibm_resource_key.service_credentials[var.service_credential_names[0].name].credentials["connection.mqtts.hosts.0.port"] : null
     credentials = {
       for service_credential in ibm_resource_key.service_credentials :
       service_credential["name"] => {
-        username = service_credential.credentials["connection.https.authentication.username"]
-        password = service_credential.credentials["connection.https.authentication.password"]
+        username = can(service_credential.credentials["connection.mqtts.authentication.username"]) ? service_credential.credentials["connection.mqtts.authentication.username"] : null
+        password = can(service_credential.credentials["connection.mqtts.authentication.password"]) ? service_credential.credentials["connection.mqtts.authentication.password"] : null
       }
     }
   } : null
 }
 
 data "ibm_database_connection" "database_connection" {
+  count         = local.is_classic ? 1 : 0
   endpoint_type = var.service_endpoints == "public-and-private" ? "public" : var.service_endpoints
   deployment_id = ibm_database.rabbitmq_database.id
   user_id       = ibm_database.rabbitmq_database.adminuser
